6e2d2701522e9c2235530ae2a819c938dbe926e7
Anton <ya.antonsakharov@yandex.ru>
2020-06-09 22:00:54 +0500

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,20 +1,32 @@
 pipeline {
     agent{node('master')}
     stages {
-    stage ('Download from git'){
-        checkout([$class: 'GitSCM',
-                    branches: [[name: '*/master']],
-                    doGenerateSubmoduleConfigurations: false,
-                    extensions: [[$class: 'RelativeTargetDirectory',
-                    relativeTargetDir: 'pipee2']],
-                    submoduleCfg: [],
-                    userRemoteConfigs: [[credentialsId: 'IlyaBurakGit',url: 'https://github.com/An1onS/study_jenkins.git']]])           
-    }
-    stage ('Run Docker'){    
-        sh 'docker build -t pipee2 -f Dockerfile .'
-        sh 'docker run -itd --name pipee2container pipee2'
-        sh 'docker exec pipee2container df -h > dfh.txt'
-        sh 'docker exec pipee2container grep cpu /proc/stat > cpu.txt'
-    }
-    
+        stage('Dowload project from github') {
+            steps {
+                script {
+                    cleanWs()                    
+                }
+                script {
+                    echo 'Downloading...'
+                    checkout([$class                           : 'GitSCM',
+                              branches                         : [[name: '*/master']],
+                              doGenerateSubmoduleConfigurations: false,
+                              extensions                       : [[$class           : 'RelativeTargetDirectory',
+                                                                   relativeTargetDir: 'pipee2']],
+                              submoduleCfg                     : [],
+                              userRemoteConfigs                : [[credentialsId: 'IlyaBurakGit', url: 'https://github.com/An1onS/study_jenkins.git']]])
+                }
+            }
+        }
+        stage ('Run'){
+            steps{
+                script{
+                    sh "docker build ${WORKSPACE}/pipee2 -t pipee2"
+                    sh "docker run -d pipee2"
+                    sh "docker exec -it pipee2 "df -h > dfh.txt""
+                    sh 'docker exec pipee2container grep cpu /proc/stat > cpu.txt'
+                }
+            }
+        }        
+    }    
 }
